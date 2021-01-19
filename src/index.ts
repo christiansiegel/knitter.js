42e7ed9958280f63f8751dc42643b7af51655ae8
Christian Siegel <christian.siegel@gmx.net>
2021-01-19 08:27:00 +0100

    introduce mobx

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,55 @@
+import { action, autorun, configure, makeAutoObservable, reaction } from 'mobx';
+
 console.log('hello world!');
 
 const worker = new Worker('worker.ts');
 worker.postMessage({});
+
+configure({
+    enforceActions: 'always',
+});
+
+class Store {
+    inputImgDataUrl?: string = undefined;
+    inputImgFile?: File = undefined;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    setInputImgDataUrl = (dataUrl: string) => (this.inputImgDataUrl = dataUrl);
+    setInputImgFile = (file: File) => (this.inputImgFile = file);
+}
+
+const store = new Store();
 
 document.addEventListener('DOMContentLoaded', () => {
     const imageUploadButton = document.getElementById('image-upload-button');
     const imageUploadInput = document.getElementById('image-upload-input');
 
     imageUploadButton?.addEventListener('click', () => imageUploadInput?.click());
-    imageUploadInput?.addEventListener('change', async (e) => {
+    imageUploadInput?.addEventListener('change', (e) => {
         const files = (<HTMLInputElement>e.target).files;
-        if (files && files.length > 0) {
-            const file = files[0];
-            const dataUrl = await readFileAsDataURL(file);
-            setInputImageSrc(dataUrl);
-        }
+        if (files && files.length > 0) store.setInputImgFile(files[0]);
     });
+
+    reaction(
+        () => store.inputImgFile,
+        async (file) => {
+            if (!file) return;
+            const dataUrl = await readFileAsDataURL(file);
+            store.setInputImgDataUrl(dataUrl);
+        },
+    );
+
+    reaction(
+        () => store.inputImgDataUrl,
+        (dataUrl) => {
+            if (!dataUrl) return;
+            const inputImage = document.getElementById('input-image');
+            if (inputImage) (<HTMLImageElement>inputImage).src = dataUrl;
+        },
+    );
 });
 
 async function readFileAsDataURL(file: File): Promise<string> {
@@ -25,9 +59,4 @@ async function readFileAsDataURL(file: File): Promise<string> {
         reader.onerror = reject;
         reader.readAsDataURL(file);
     });
-}
-
-function setInputImageSrc(src: string): void {
-    const inputImage = <HTMLImageElement | null>document.getElementById('input-image');
-    if (inputImage) inputImage.src = src;
 }
